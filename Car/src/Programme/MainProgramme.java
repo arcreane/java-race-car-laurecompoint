9174d4c527dc32d45e3007a910b7d9a9ec2f5c23
Laure C <compointlaure@gmail.com>
2020-04-07 14:19:07 +0200

    temps de parcoure de la voiture pour faire les 50km

--- Car/src/Programme/MainProgramme.java
+++ Car/src/Programme/MainProgramme.java
@@ -42,22 +42,31 @@ public class MainProgramme {
 		  case 1:
 			  System.out.println("Vous avez choisie une voiture rapide");
 			 
-			
+			 long timestartracefast = System.currentTimeMillis();
 			 car.add(CarFast.createcarfast());
 			 motor.add(MotorRapide.motorrapide());
-			 System.out.println("Vous voiture commence a roulez avec une certain vitesse, choisisez la vitesse de votre voiture ?");
-			 System.out.println("Attention, selon la vitese choisie vous risque de perdre des pieces...");
+			 System.out.println("Vous voiture commence a roulez avec une certain vitesse");
+			 System.out.println("Attention, vous avez 60% de chance de perdre des pieces pendant la courses.");
 			 car.add(CarFast.speedpower());
+			 long timeendracefast = System.currentTimeMillis();
+			 float timeracefast = (timeendracefast - timestartracefast) / 1000F;
+			 System.out.println("Votre voiture à fini la course et à parcouru 50km en " + timeracefast  + " seconde");
+			 System.out.println("Fin du jeu");
 			 
 		    break;
 		  case 2:
 			  System.out.println("Vous avez choisie une voiture lente");
+			  long timestartraceslow = System.currentTimeMillis();
 			  car.add(CarSlow.createcarslow());
 			  motor.add(MotorSlow.motorslow());
 			  System.out.println("Vous voiture commence a roulez avec une certain vitesse, choisisez la vitesse de votre voiture ?");
-			  System.out.println("Attention, selon la vitese choisie vous avez le droit un boost de vitesse.");
+			  System.out.println("Attention, avec avez 30% de chance d'essayer d'avoir un boost de vitesse pour vous aider pendant la courses");
 			  car.add(CarSlow.speedpower());
-			 
+			  long timeendraceslow = System.currentTimeMillis();
+			  float timeraceslow = (timeendraceslow - timestartraceslow) / 1000F;
+			  System.out.println("Votre voiture à fini la course et à parcouru 50km en " + timeraceslow + " seconde");
+			  System.out.println("Fin du jeu");
+				 
 			  
 			 
 			  
